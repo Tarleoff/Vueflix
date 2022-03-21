8852263d88545e4f988b458f911e55712c931db0
Gemma <a201836gm@correu.escoladeltreball.org>
2022-03-21 16:16:35 +0100

    up

---
+++ src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Login</h1>
+  <input type="email" placeholder="Email" />
+  <input type="password" placeholder="Password" />
+  <button value="Login">Log in</button>
+</template>
+
+<script>
+export default{
+    
+}
+</script>
+
+<style scoped>
+</style>
